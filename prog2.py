b5c10a73e832525820455474123897ae335b9e0e
ghost401 <b.kotskovych@gmail.com>
2017-08-22 22:38:28 +0300

    Update prog2.py

--- prog2.py
+++ prog2.py
@@ -1,91 +1,114 @@
-import requests
-from bs4 import BeautifulSoup
 import sqlite3
+import requests
+import csv
+import re
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from datetime import datetime
 
 class Database():
 
     def __init__(self):
-        con=sqlite3.connect("parse.db")
-        cur=con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS npdb (NAME INT, PHOTO TEXT, LINK TEXT, PRICE INT, DETAILS INT)")
-        con.commit()
-        con.close()
+        self.conn = sqlite3.connect("result.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute("DROP TABLE if EXISTS pudb")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS pudb (name text, price integer, link text, image text, details text)")
+        self.conn.commit()
 
-    def todb(self, name, image, link, price, details):
-        con=sqlite3.connect("parse.db")
-        cur=con.cursor()
-        cur.execute("INSERT INTO npdb (?, ?, ?, ?, ?)",(name, image, link, price, details))
-        con.commit()
-        con.close()
+    def insert(self, name, price, link, image, details):
+        self.cur.execute("INSERT INTO pudb VALUES (?, ?, ?, ?, ?)",(name, price, link, image, details))
+        self.conn.commit()
 
     def view(self):
-        con=sqlite3.connect("parse.db")
-        cur=con.cursor()
-        cur.execute("SELECT * FROM npdb)
-        rows = cur.fetchall()
-        con.close()
+        self.cur.execute("SELECT * FROM pudb")
+        rows = self.cur.fetchall()
         for row in rows:
-            print row
+            print(row)
+
+    def __del__(self):
+        self.conn.close()
+
 class PUParser():
-	
     def conn(self, link):
-        try:
-            cont = requests.get(link)
-        except HTTPError:
-            print("Can not access server!")
+        cont = requests.get(link)
         soup = BeautifulSoup(cont.content, "html.parser")
         return soup
-		
-    def cr_links(self, i):
-        base = 'http://price.ua/catc839t14/page'
-        ad = '.html'
-        link = base + str(i) + ad
+
+    def cr_links(self, i, min_price, max_price):
+        """creates urls to parse"""
+        link = "http://price.ua/catc839t1/page" + str(i) + ".html?price[min]=" + min_price + "&price[max]=" + max_price + ".html"
         return link
 
-    def get_amount(self):
-        try:
-            s_data = requests.get("http://price.ua/catc839t14.html")
-        except HTTPError:
-            print("Can not access server!")
-        soup = BeautifulSoup(s_data.content, "html.parser")
-        soup = soup.find("span",  id = "top-paginator-max")
-        amount = int(soup.get_text())
+    def get_amount(self, soup):
+        """gets number of the last page"""
+        amount = soup.find("span", id="top-paginator-max")
+        amount = int(amount.get_text())
         return amount
-	
-    def get_b(self, html):
-        new_b = html.find_all("div",  class_ = 'product-item')
-        return new_b
-    
-    def parse(self, b, i):
-        name = b[i].find("a",  class_ = 'model-name')
-        name = name.get('title')
-        name = name.replace('Цены на ', '')
-        name = name.replace(' в Ивано-Франковске', '')
-        price = b[i].find("span",  class_ = "price")
-        price = price.get_text()
-        price = price.replace('\xa0', '')
-        price = price.replace('грн.', '')
-        price = price.replace(' ', '')
-        price = int(price)
-        det = b[i].find_all("div",  class_ = 'item')
-        details = ""
-        for i in range(len(det)):
-            details += str(det[i].get_text())
-        link = b[i].find("a",  class_ = 'model-name')
-        link = link.get('href')
-        image = b[i].find('img')
-        image = image.get('src')
-        if price > 10000 and price < 20000:
-            db.todb(name, image, link, price, details)
-pp = PUParser()
-db = Database()
+
+    def get_wrap(self, html):
+        wrap_list = html.find_all("div", class_='product-item')
+        return wrap_list
+
+    def parse(self, wraps, i, f):
+        price_wrap = wraps[i].find("div", class_="price-wrap")
+        price_wrap = price_wrap.get_text().replace('\xa0', '')
+        numbers = re.findall('(\d+)', price_wrap)
+        price = int("".join(numbers))
+        try:
+            name = wraps[i].find("a", class_='model-name')
+            name = name.get('title').replace('Цены на ', '').replace(' в Ивано-Франковске', '')
+        except AttributeError:
+            name = wraps[i].find("a", class_='ga_card_simple_pic')
+            name = name.get('title')
+            name_ar = name.split(" ")
+            name_ar = name_ar[1: -2]
+            name = " ".join(name_ar)
+        image = wraps[i].find('img')
+        image = image.get('data-original')
+        try :
+            link = wraps[i].find("a", class_='model-name')
+            link = link.get('href')
+        except AttributeError:
+            link2 = wraps[i].find("a", class_='ga_card_simple_pic')
+            link = link2.get('href')
+        try:
+            det_page_soup = pu.conn(link)
+            det_n  = det_page_soup.find_all("td", class_ = "td-name")
+            det = det_page_soup.find_all("td", class_ = "td-value")
+            details = ""
+            for i in range(1, len(det_n) - 2):
+                details += det_n[i].get_text() + " " + det[i].get_text() + " "
+        except:
+            details = " "
+        db.insert(name, price, link, image, details)
+        to_csv(f, name, price, link, image, details)
+        print(price, name, image, link, details)
+
+
+def to_csv(f, name, price, link, image, details):
+    writer = csv.writer(f)
+    writer.writerow((name, price, link, image, details))
 
 def main():
-    for i in range(pp.get_amount()):
-        link = pp.cr_links(i)
-        html = pp.conn(link)
-        for j in range(len(pp.get_b(html))):
-            b = pp.get_b(html)
-            pp.parse(b, i)
-
-main()
+    min_price = str(input('input min price'))
+    max_price = str(input("input max price"))
+    a = datetime.now()
+    print(a)
+    f = open("result.csv", 'w')
+    max = pu.get_amount(pu.conn(pu.cr_links(1, min_price, max_price)))
+    print(max)
+    for i in range(1, 10):
+        print(i + 1)
+        wraps = pu.get_wrap(pu.conn(pu.cr_links(i, min_price, max_price)))
+        for j in range(len(wraps)):
+            print(j + 1)
+            pu.parse(wraps, j, f)
+    b = datetime.now()
+    db.view()
+    print(a)
+    print(b)
+    f.close()
+pu = PUParser()
+db = Database()
+if __name__ == '__main__':
+    main()
